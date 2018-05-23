59d9a30d70b26047cbf52d80c70de8f0c5a729f5
Tonnerre Lombard <tonnerre@ancient-solutions.com>
2018-05-23 16:45:12 +0200

    Also wipe out the agreement_pdf when deleting a member.

--- db.go
+++ db.go
@@ -36,8 +36,9 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"errors"
-	"github.com/golang/protobuf/proto"
 	"time"
+
+	"github.com/golang/protobuf/proto"
 )
 
 // Data used by the HTML template. Contains not just data entered so far,
@@ -77,7 +78,7 @@ var allColumns [][]byte = [][]byte{
 	[]byte("phone"), []byte("fee"), []byte("username"), []byte("pwhash"),
 	[]byte("fee_yearly"), []byte("sourceip"), []byte("useragent"),
 	[]byte("metadata"), []byte("pb_data"), []byte("application_pdf"),
-	[]byte("approval_ts"),
+	[]byte("agreement_pdf"), []byte("approval_ts"),
 }
 
 // Create a new connection to the membership database on the given "host".
